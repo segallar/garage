6e44c3d1cd51bafadb5edcff7f91b587bb052c8b
Roman Osipov <roman@segalla.ru>
2015-11-08 19:05:02 +0300

    debug

--- server.php
+++ server.php
@@ -34,21 +34,22 @@ function events_cols() {
 }
 
 function events_where() {
-    $WHERE = "";
+    $where = "";
     
-    if($debug) $WHERE = " xxx ";
+    if($debug) $where .= " xxx ";
     
     foreach($params as &$param) {
         if($debug) echo "param $param \n";
         if(isset($_GET[$param])&&$_GET[$param]!="") {
-            if($WHERE!="")
-                $WHERE .= " AND ";
-            $WHERE .= " $param(ts) = $_GET[$param] ";
+            if($where!="")
+                $where .= " AND ";
+            $where .= " $param(ts) = $_GET[$param] ";
         }
     }
-    if($WHERE!="") 
-        $WHERE = " WHERE $WHERE";
-    return $WHERE;
+    if($where!="") 
+        $where = " WHERE $where";
+    
+    return $where;
 }
 
 switch ( $cmd ) {
@@ -95,65 +96,16 @@ case "events":
         if (!$result) {
             die(json_encode('Invalid query: ' . mysql_error()));
         } else {
-            $arr2 = [];
             while($arr = mysql_fetch_array($result, MYSQL_ASSOC)) {
-                $arr2[] = $arr;
+                $return[] = $arr;
             }     
-            $return = $arr2;
         }
     }
     catch (Exception $e) {
         $e->getMessage();
     }
     break;
-case "get_track":
-    /* GeoJSON example 
-    { "type": "MultiLineString",
-    "coordinates": [
-        [ [100.0, 0.0], [101.0, 1.0] ],
-        [ [102.0, 2.0], [103.0, 3.0] ]
-      ]
-    } */
-    try {
-        $db = mysql_connect($mysql_host, $mysql_user, $mysql_password) or 
-            die(json_encode("Database error")); 
-        mysql_select_db($mysql_database, $db); 
 
-        //SOLUTION::  add this comment before your 1st query -- force multiLanuage support 
-        //$result = mysql_query("set names 'utf8'"); 
-
-        $return = [];
-        $return['type'] = "MultiLineString";
-        $return['coordinates'] = [];
-        $first = true;
-        $prevLon = 0;
-        $prevLat = 0;
-
-        $session = $_GET["session"];
-        if(isset($session)&&$session!="") {
-            $query = "select lat, lon from track where session='".$session."' order by id;";
-            $result = mysql_query($query);
-            if (!$result) {
-                die(json_encode('Invalid query: ' . mysql_error()));
-            } else {
-                while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-                    $lon = floatval($row['lon']);
-                    $lat = floatval($row['lat']);
-                    if($first) {
-                        $first = false;
-                    } else {
-                        $return['coordinates'][] = [[$prevLon,$prevLat],[$lon,$lat]];
-                    }
-                    $prevLon = $lon;
-                    $prevLat = $lat;
-                }
-            }
-        }
-    }
-    catch (Exception $e) {
-        $e->getMessage();
-    }
-    break;
 default :
     $return["cmd"] = "no_command"; 
 }
