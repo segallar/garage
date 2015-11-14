80ab2ad8296433dcd4829fd8e552aa0b73adc6c4
Roman Segalla <roman@segalla.ru>
2015-11-14 13:02:50 +0300

    session test

--- info.php
+++ info.php
@@ -2,11 +2,18 @@
 
 //phpinfo();
 
- 
 session_start(); 
+
+if(isset($_GET['cmd'])&&$_GET['cmd']=='destroy') {
+  session_destroy();
+  header("Location: http://".$_SERVER['HTTP_HOST']."/");
+  exit;
+}
+
 if (!isset($_SESSION['counter'])) $_SESSION['counter']=0;
 echo "Вы обновили эту страницу ".$_SESSION['counter']++." раз. ";
 echo "<br><a href=".$_SERVER['PHP_SELF'].">обновить"; 
 
+echo "<br><a href=".$_SERVER['PHP_SELF']."?cmd=destroy>Удалить сессию</a>";
 
 ?>
