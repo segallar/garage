683b3c41101bf6e0d411045a58f8791fc8fbe976
Roman Segalla <roman@segalla.ru>
2015-11-14 12:56:54 +0300

    session test

--- info.php
+++ info.php
@@ -1,5 +1,12 @@
 <?php 
 
-phpinfo();
+//phpinfo();
+
+ 
+session_start(); 
+if (!isset($_SESSION['counter'])) $_SESSION['counter']=0;
+echo "Вы обновили эту страницу ".$_SESSION['counter']++." раз. ";
+echo "<br><a href=".$_SERVER['PHP_SELF'].">обновить"; 
+
 
 ?>
