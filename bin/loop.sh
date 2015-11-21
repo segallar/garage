df25765adf061fc39e59265158a4e381f2dc674e
Roman Osipov <roman@segalla.ru>
2015-11-22 01:12:07 +0300

    i2c_test

--- bin/loop.sh
+++ bin/loop.sh
@@ -2,6 +2,6 @@
 #
 # garage project 
 #
-php /home/pi/garage/bin/LLS331AP_sql.php `/home/pi/garage/bin/LLS331AP.sh` 
-
+#php /home/pi/garage/bin/LLS331AP_sql.php `/home/pi/garage/bin/LLS331AP.sh` 
+/home/pi/garage/bin/i2c_test
 
