125b0d6ffc159fdebd89f1816d79a2a033a61c73
Roman Segalla <roman@segalla.ru>
2015-11-25 17:02:44 +0300

    1

--- src/i2c_test2.cpp
+++ src/i2c_test2.cpp
@@ -49,10 +49,6 @@ int main(void)
   opResult = read(i2cHandle, rxBuffer, 1);
   printf("Part ID: 0x%x\n", (int)rxBuffer[0]); // should print 105
 
-  // Clear our buffers
-  memset(rxBuffer, 0, sizeof(rxBuffer));
-  memset(txBuffer, 0, sizeof(txBuffer));
-    
   //*** write !!! 0x20, 0x90
   txBuffer[0] = 0x20; // This is the address we want to read from.
   opResult = write(i2cHandle, txBuffer, 1);
@@ -61,13 +57,6 @@ int main(void)
   opResult = write(i2cHandle, txBuffer, 1);
   if (opResult != 1) printf("No ACK bit 3!\n");
 
-    
- // printf("%i \n",i2c_smbus_write_byte_data(i2cHandle, 0x20, 0x90));        
-   
-  memset(rxBuffer, 0, sizeof(rxBuffer));
-  memset(txBuffer, 0, sizeof(txBuffer));
-   
-  opResult = ioctl(i2cHandle, I2C_SLAVE, barometerAddress);    
   //*** read !!! 0x28
   txBuffer[0] = 0x28; // This is the address we want to read from.
   opResult = write(i2cHandle, txBuffer, 1);
